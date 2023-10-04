c6c494af6603a269183061d66beab57b0125cc9c
Boris <b.edyson.12@gmail.com>
2023-10-04 08:38:03 +0300

    add base classes attributes

--- src/lab01/Faculty.java
+++ src/lab01/Faculty.java
@@ -1,4 +1,10 @@
 package lab01;
+import java.util.List;
 
 public class Faculty {
+    private String name;
+    private String abbreviation;
+    private List<Student> students;
+    private StudyField studyField;
+
 }
--- src/lab01/Student.java
+++ src/lab01/Student.java
@@ -1,4 +1,12 @@
 package lab01;
+import java.time.LocalDate;
 
 public class Student {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private LocalDate enrollmentDate;
+    private LocalDate dateOfBirth;
+    private boolean graduated;
+
 }
