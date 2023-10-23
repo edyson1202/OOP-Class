526bc0e2e295f0afc239bc61cc3c7c76533cb737
Boris <b.edyson.12@gmail.com>
2023-10-23 10:32:49 +0300

    add more functionality

--- src/lab01/Student.java
+++ src/lab01/Student.java
@@ -6,7 +6,28 @@ public class Student {
     private String lastName;
     private String email;
     private LocalDate enrollmentDate;
-    private LocalDate dateOfBirth;
+    private LocalDate birthDate;
     private boolean graduated;
+
+    public Student (String firstName, String lastName, String email, LocalDate enDate, LocalDate birthDate, boolean graduated) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.enrollmentDate = enDate;
+        this.birthDate = birthDate;
+        this.graduated = graduated;
+    }
+
+    @Override
+    public String toString() {
+        return firstName + " " + lastName + "\n";
+    }
+    public String getFirstName() { return this.firstName; }
+    public String getLastName() { return this.lastName; }
+    public String getEmail() { return this.email; }
+    public LocalDate getEnrollmentDate() { return this.enrollmentDate; }
+    public LocalDate getBirthDate() { return this.birthDate; }
+    public boolean getGraduated() { return this.graduated; }
+    public void setGraduated(boolean value) { this.graduated = value; }
 
 }
